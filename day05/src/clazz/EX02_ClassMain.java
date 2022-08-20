af5bbccfc7c9a5d55d358e1619529031f927c29f
selimssy <selimssy@naver.com>
2022-08-20 14:57:00 +0900

    obj

---
+++ day05/src/clazz/EX02_ClassMain.java
@@ -0,0 +1,34 @@
+package clazz;
+
+
+
+class Person{
+	
+	// 필드(객체의 속성)
+	String name;
+	int age;
+	
+	// 생성자(객체 초기화)
+	public Person() {}
+	
+	
+	// 메소드(객체의 기능)
+	public void walk() {}
+	public void run() {}
+	
+}
+
+
+
+
+public class EX02_ClassMain {
+
+	public static void main(String[] args) {
+		
+		Person person1 = new Person();
+     //클래스타입 클래스변수  new 생성자호출
+		
+		
+ 	}
+
+}
---
+++ day05/src/clazz/Ex1_Hello.java
@@ -0,0 +1,14 @@
+package clazz;
+
+
+
+// 소스파일의 이름은 public class 이름과 일치
+// 하나의 소스파일에는 하나의 public class만 허용
+// public class class가 아니면 하나의 소스파일에 여러 class 만들 수 있지만 하나의 class만 만드는게 좋다
+public class Ex1_Hello {}
+
+
+
+
+
+class Hello3{}
